892ecce9189e7fc01edfff51482aeaf80f35c451
btheodorou <btheodorou@g.hmc.edu>
2020-10-07 13:48:01 -0700

    Completed framework for adding and using solver, just needing compiled dds

--- games/double-dummy/solver.jl
+++ games/double-dummy/solver.jl
@@ -9,13 +9,15 @@ import ..Game, ..history, ..WHITE
 import AlphaZero: GI, GameInterface, Benchmark, AbstractPlayer, think
 
 const DEFAULT_SOLVER_DIR = joinpath(@__DIR__, "solver", "dds") # TODO
+const leader_dict = Dict([(0, "N"), (1, "E"), (2, "S"), (3, "W")])
+const rank_dict = Dict([(1, "2"), (2, "3"), (3, "4"), (4, "5"), (5, "6"), (6, "7"), (7, "8"), (8, "9"), (9, "T"), (10, "J"), (11, "Q"), (12, "K"), (13, "A")])
 
 struct Player <: AbstractPlayer{Game}
   process :: Base.Process
   lock :: ReentrantLock
   function Player(;
       solver_dir=DEFAULT_SOLVER_DIR,
-      solver_name="c4solver", # TODO
+      solver_name="ddsolver",
       disable_stderr=true)
     cmd = Cmd(`./$solver_name`, dir=solver_dir)
     if disable_stderr
@@ -31,9 +33,40 @@ end
 # - Standard output: space separated optimal action numbers
 
 function hand_string(p::Player, g)
-    board = GI.current_state(g).board
-    return "" # TODO
+  # Extract the board from the game
+  board = GI.current_state(g).board
+  
+  # Calculate the trump suit
+  if isnothing(findfirst(iszero, board[:,:,5]))
+    trump = 5
+  else
+    trump = findfirst(x -> x == 1, board[:,:,5])[2]
+  end
+  
+  # Calculate which player is up next
+  first = maximum(g.board[:,:,6])
+  
+  # Calculate the cards that have been played in the current trick
+  ct_ranks = zeros(UInt8, 3)
+  ct_suits = zeros(UInt8, 3)
+  if first > 0
+    for i in 1:first
+      card = findfirst(x -> x == i, board[:,:,6])
+      ct_ranks[i] = card[1]
+      ct_suits[i] = card[2]
+    end
+  end
+
+  # Create the PBN representation of the remaining hands
+  pbn = string("$(get(leader_dict, first, "")):$(join([join([join([get(rank_dict, index[2], "") for index in findall(board[:,i,j])]) for i in 1:4], ".") for j in 1:4], " "))")
+
+  # Return the string in the proper format
+  return "$pbn-$trump-$first-$(ct_suits[1])-$(ct_ranks[1])-$(ct_suits[2])-$(ct_ranks[2])-$(ct_suits[3])-$(ct_ranks[3])"
 end
+
+card1 = findfirst(x -> x == 1, board[:,:,6])
+winning_value = leading_card[1]
+winning_suit = leading_card[2]
 
 function query_solver(p::Player, g)
   hstr = hand_string(g)
