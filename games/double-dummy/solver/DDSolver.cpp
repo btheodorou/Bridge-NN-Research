40a29a56b3b1570d7786032583f605b7ad7e1a11
btheodorou <btheodorou@hmc.edu>
2020-10-14 18:07:00 -0700

    Trying to get DDSolver up and running

---
+++ games/double-dummy/solver/DDSolver.cpp
@@ -0,0 +1,66 @@
+/*
+   Using DDS, a bridge double dummy solver.
+   Copyright (C) 2006-2014 by Bo Haglund /
+   2014-2016 by Bo Haglund & Soren Hein.
+   See LICENSE and README.
+
+   This file written by Brandon Theodorou
+   to provide a command line interface for
+   use in AlphaZero Double Dummy Agent
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include "../include/dll.h"
+
+int main()
+{
+  int target = -1;
+  int solutions = 2;
+  int mode = 0;
+  int threadIndex = 0;
+  char line[80];
+
+#if defined(__linux) || defined(__APPLE__)
+  SetMaxThreads(0);
+#endif
+
+  while (true)
+  {
+    // Initialize for the latest command line input
+    std::string dealEncoding;
+    dealPBN dlPBN;
+    futureTricks fut;
+
+    // Extract the information
+    std::getline(std::cin, dealEncoding, '-');
+    strcpy(dlPBN.remainCards, &dealEncoding[0]);
+    std::cout << dlPBN.remainCards << std::endl;
+    std::getline(std::cin, dealEncoding, '-');
+    dlPBN.trump = std::stoi(dealEncoding, nullptr);
+    std::cout << dlPBN.trump << std::endl;
+    std::getline(std::cin, dealEncoding, '-');
+    dlPBN.first = std::stoi(dealEncoding, nullptr);
+    dlPBN.currentTrickSuit[0] = std::stoi(strtok(NULL, "-"), nullptr);
+    dlPBN.currentTrickRank[0] = std::stoi(strtok(NULL, "-"), nullptr);
+    dlPBN.currentTrickSuit[1] = std::stoi(strtok(NULL, "-"), nullptr);
+    std::cout << dlPBN.currentTrickSuit[1];
+    dlPBN.currentTrickRank[1] = std::stoi(strtok(NULL, "-"), nullptr);
+    dlPBN.currentTrickSuit[2] = std::stoi(strtok(NULL, "-"), nullptr);
+    dlPBN.currentTrickRank[2] = std::stoi(strtok(NULL, "-"), nullptr);
+
+    // Solve the board
+    SolveBoardPBN(dlPBN, target, solutions, mode, &fut, threadIndex);
+
+    // Print the optimal moves
+    for(int action = 0; action < fut.cards; action++)
+    {
+      int actionNumber = (fut.suit[action] * 13) + (fut.rank[action] - 2) + 1;
+      std::cout << actionNumber << " ";
+    }
+  }
+}
