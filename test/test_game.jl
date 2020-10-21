d06ae44758c84daf3c898fd2eacf021045657637
btheodorou <btheodorou@g.hmc.edu>
2020-10-20 21:22:52 -0700

    Fixed issue with reneges in double dummy game code

--- test/test_game.jl
+++ test/test_game.jl
@@ -8,7 +8,7 @@ function generate_states(::Type{Game}, n) where Game
   traces = []
   player = RandomPlayer{Game}()
   for i in 1:n
-    trace = play_game(player)
+    _, trace = play_game(player)
     push!(traces, trace)
   end
   return Set(s for t in traces for s in t.states)
