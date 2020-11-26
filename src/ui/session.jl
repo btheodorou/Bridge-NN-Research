51a2937c006766f6fd2a2ab84f4f917160199852
btheodorou <btheodorou@g.hmc.edu>
2020-11-25 23:35:47 -0700

    Added custom training

--- src/ui/session.jl
+++ src/ui/session.jl
@@ -681,7 +681,7 @@ function train_and_monitor(::Type{G}, session_dir, label, maxEpochs, benchmarks,
 
   # Instantiate the network
   # network = load_network(Logger(), "", net_params_file)
-  network = Netword(netparams)
+  network = Network(netparams)
   
   # Load the memory
   mem_file = joinpath(session_dir, MEM_FILE)
